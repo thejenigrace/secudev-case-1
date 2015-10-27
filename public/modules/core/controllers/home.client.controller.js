20814abe38eb8cbf70f084100ad440a9ab2b0d84
thexilent13 <jennicaalcalde@yahoo.com>
2015-10-27 22:47:27 +0800

    Merge remote-tracking branch 'remotes/origin/cycle-4-jeca' into cycle-4-initial
    # Conflicts:
    #	bower.json
    #	config/env/production.js
    #	config/env/secure.js
    #	public/modules/admin/config/admin.client.config.js

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		//$scope.user = Authentication.user;
+        //
+		//// If user is signed in then redirect back to /board/posts
+		//if (!$scope.user) $location.path('/');
+		//else $location.path('/board/posts');
 	}
 ]);
